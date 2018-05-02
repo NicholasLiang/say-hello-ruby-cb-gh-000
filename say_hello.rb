2f5dcc9613806357955e46cfcce14e5a46935222
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 05:57:11 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,6 @@
 # Build your say_hello method here
 
-def say_hello(name)
-  var = "Hello Ruby Programmer!"
-  if name = NIL
+def say_hello(name = Ruby Programmer)
+  var = "Hello !"
   return "Hello " + name + "!"
 end
